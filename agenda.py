33d17e61139be86327cb26f1f21c79342a94e7cf
iarakelly <iarakelly.ba@gmail.com>
2024-09-10 13:59:57 -0300

    menu

---
+++ agenda.py
@@ -0,0 +1,32 @@
+
+lista = []
+
+def menu ():
+    print('''
+==============Agenda de Contatos==============
+[1]Adicionar
+[2]Listar
+[3]Buscar
+[4]Atualizar
+[5]Exluir
+==============================================''')
+    opc = input("Selecione uma opeção: ")
+    if opc == "1":
+        #criar contato
+        pass
+    elif opc == "2":
+        #listar contatos
+        pass
+    elif opc == "3":
+        #buscar contatos
+        pass
+    elif opc == "4":
+        #att contato
+        pass
+    elif opc == "5":
+        #delete contato
+        pass
+    
+def main():
+    menu()
+main()
